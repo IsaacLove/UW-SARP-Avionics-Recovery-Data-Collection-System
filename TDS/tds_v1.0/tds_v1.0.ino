d43ca28e30f63b7d4437ac624780a6bf5070b59c
TarkanAl-Kazily <tarkucar@mac.com>
2016-05-18 16:19:54 -0700

    Added code and example code for the nosecone temperature sensor using the OneWire and DallasTemperature libararies.

--- TDS/tds_v1.0/tds_v1.0.ino
+++ TDS/tds_v1.0/tds_v1.0.ino
@@ -59,7 +59,16 @@
 #define ACCEL_Z A0
 
 // Nosecone Temperature Sensor (DS18B20)
-#define TEMP_DQ 6 // @TODO: finalize pins
+#define TEMP_DQ_BUS 6 // @TODO: finalize pins
+
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(TEMP_DQ_BUS);
+
+// Pass our oneWire reference to Dallas Temperature.
+DallasTemperature sensors(&oneWire);
+
+// arrays to hold device address
+DeviceAddress insideThermometer;
 
 ////////////////////////////////////////////////////////////////////////////////
 // SPI devices:
@@ -162,6 +171,8 @@ void setup() {
         Serial.println("#RocketName: ERROR4: NO GPS DETECTED");
     }
 
+    temperatureSetup();
+
     Serial.println("TDS is Online");
 }
 
@@ -177,6 +188,16 @@ void loop() {
     logData(barometer_data);
     //Serial.println(gps_data);
     logData(gps_data);
+
+    // call sensors.requestTemperatures() to issue a global temperature
+    // request to all devices on the bus
+    Serial.print("Requesting temperatures...");
+    sensors.requestTemperatures(); // Send the command to get temperatures
+    Serial.println("DONE");
+
+    // It responds almost immediately. Let's print out the data
+    String temperature_data = getTemperatureData(insideThermometer); // Use a simple function to print out the data
+
     smartdelay(DELAY - (millis() - milli));
 }
 
@@ -237,7 +258,7 @@ float getAltitude()
 String getBaromData()
 {
     return "Temp: " + (String)(getTemperature()) + ", " + "Pascal: " + (String)(getPressure())
-        + ", " + "Alt: " + (String)(getAltitude());
+    + ", " + "Alt: " + (String)(getAltitude());
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -288,7 +309,7 @@ String getAccelData()
     float z_accel = getAccelGs(getRawAccelZ());
 
     return "X_Accel: " + (String)(x_accel) + ", " + "Y_Accel: " + (String)(y_accel)
-        + ", " + "Z_Accel: " + (String)(z_accel);
+    + ", " + "Z_Accel: " + (String)(z_accel);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -318,7 +339,7 @@ String getGPSLocation ()
     //Serial.println("----" + (String)lon);
     //Serial.println("----" + (String)age);
     return "Lat: " + (String)(lat) + ", " + "Long: " + (String)(lon) + ", " +
-        "Age: " + (String)(age);
+    "Age: " + (String)(age);
 }
 
 String getGPSSpeed()
@@ -343,12 +364,12 @@ String getGPSDateTime()
 
 static void smartdelay(unsigned long ms)
 {
-  unsigned long start = millis();
-  do
-  {
-    while (Serial1.available())
-      gps.encode(Serial1.read());
-  } while (millis() - start < ms);
+    unsigned long start = millis();
+    do
+    {
+        while (Serial1.available())
+        gps.encode(Serial1.read());
+    } while (millis() - start < ms);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -400,4 +421,69 @@ void setLongitude(String longitude)
     str += longitude;
 
     Serial2.print(str);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// One-Wire Temperature Sensor
+// Mounted in the Nosecone
+////////////////////////////////////////////////////////////////////////////////
+
+void temperatureSetup() // @TODO: Make Serial messages toggleable
+{
+    // locate devices on the bus
+    Serial.print("Locating One-Wire devices...");
+    sensors.begin();
+    Serial.print("Found ");
+    Serial.print(sensors.getDeviceCount(), DEC);
+    Serial.println(" One-Wire devices.");
+
+    // report parasite power requirements
+    Serial.print("Parasite power is: ");
+    if (sensors.isParasitePowerMode()) Serial.println("ON");
+    else Serial.println("OFF");
+
+    // Method 1:
+    // Search for devices on the bus and assign based on an index. Ideally,
+    // you would do this to initially discover addresses on the bus and then
+    // use those addresses and manually assign them (see above) once you know
+    // the devices on your bus (and assuming they don't change).
+    if (!sensors.getAddress(insideThermometer, 0)) Serial.println("Unable to find address for Device 0");
+
+    // show the addresses we found on the bus
+    Serial.print("Device 0 Address: ");
+    printAddress(insideThermometer);
+    Serial.println();
+
+    // set the resolution to 9 bit (Each Dallas/Maxim device is capable of several different resolutions)
+    sensors.setResolution(insideThermometer, 9);
+
+    Serial.print("Device 0 Resolution: ");
+    Serial.print(sensors.getResolution(insideThermometer), DEC);
+    Serial.println();
+}
+
+// function to print the temperature for a device
+String getTemperatureData(DeviceAddress deviceAddress)
+{
+    // method 1 - slower
+    //Serial.print("Temp C: ");
+    //Serial.print(sensors.getTempC(deviceAddress));
+    //Serial.print(" Temp F: ");
+    //Serial.print(sensors.getTempF(deviceAddress)); // Makes a second call to getTempC and then converts to Fahrenheit
+
+    // method 2 - faster
+    float tempC = sensors.getTempC(deviceAddress);
+    return "Temp C: " + (String) tempC;
+    //Serial.print(" Temp F: ");
+    //Serial.println(DallasTemperature::toFahrenheit(tempC)); // Converts tempC to Fahrenheit
+}
+
+// function to print a device address
+void printAddress(DeviceAddress deviceAddress)
+{
+    for (uint8_t i = 0; i < 8; i++)
+    {
+        if (deviceAddress[i] < 16) Serial.print("0");
+        Serial.print(deviceAddress[i], HEX);
+    }
 }
